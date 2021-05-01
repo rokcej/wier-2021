b9119b49c6afbc9cf5b7e541c7cbfc04f6425611
rokcej <rokcej1997@gmail.com>
2021-05-01 16:54:47 +0200

    Fixed regex extraction

--- pa2/README.md
+++ pa2/README.md
@@ -1,16 +1,27 @@
 # Programming Assignment 2
 
+The goal of this assignment is to use three different approaches for HTML data extraction: regular expressions, XPath and RoadRunner.
+
+The data is extracted from three chosen websites: overstock.com, rtvslo.si and manazara.si.
+
 ## Prerequisites
+
+Install Python 3 and the following packages:
+
 ```bash
 pip install beautifulsoup4
 pip install lxml
+pip install unidecode
 ```
 
 ## Usage
+
+To run the program, move into the `input-extraction` directory, then run `run-extraction.py` with different parameters.
+
 ```bash
 cd input-extraction
-python ./run-extraction.py A
-python ./run-extraction.py B
-python ./run-extraction.py C
+python ./run-extraction.py A # Regex extraction
+python ./run-extraction.py B # XPath extraction
+python ./run-extraction.py C # RoadRunner extraction
 ```
 
--- pa2/implementation-extraction/regex_extraction.py
+++ pa2/implementation-extraction/regex_extraction.py
@@ -1,335 +1,247 @@
-# Extract data from overstock.com
-def processOverstock(html_content):
-	# Import libraries
-
-import re
-import json
-
-
-# We rather use the locally-cached file as it may have changed online.
-pageContent = open(html_content, 'r').read()
-Description={}
-LastPrice={}
-Price={}
-Saving={}
-SavingPercent={}
-Title={}
-i=0
-
-#TITLE
-i=0
-regex = r"<a href=(\")http:(\/\/)www.overstock.com(\/)cgi-bin(\/)d2.cgi(\?)PAGE=PROFRAME(\&)amp;PROD_ID=(\d{5,7})(\")><b>(.*)</b>"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    Title[i] = match.group(9)
-    i=i+1
-
-#DESCRIPTION
-i=0
-regex=r"(?:<td valign=\"top\"><span class=\"normal\">)((.*?)[\s\S]*?)<br>(.*?)\"><span class=\"tiny\"><b>(.*)<\/b>"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    Description[i] = str(match.group(1)+match.group(4)).replace("\n","")
-    i=i+1
-
-#LAST PRICE
-i=0
-regex=r"<tbody><tr><td align=\"right\" nowrap=\"nowrap\"><b>(.*)<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><s>(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5}))<\/s><\/td><\/tr>"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    LastPrice[i] = match.group(2)
-    i=i+1
-
-
-#REGULAR PRICE
-i=0
-regex=r"<tr><td align=\"right\" nowrap=\"nowrap\"><b>Price:<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><span class=\"bigred\"><b>(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5}))<\/b><\/span><\/td><\/tr>"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    Price[i] = match.group(1)
-    i=i+1
-
-#SAVED
-i=0
-regex=r"<tr><td align=\"right\" nowrap=\"nowrap\"><b>You Save:<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><span class=\"littleorange\">(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5})) (\()[0-9][0-9](\%)(\))<\/span><\/td><\/tr>"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    Saving[i] = match.group(1)
-    i=i+1
-
-#SAVED IN PERCENT
-i=0
-regex=r"<tr><td align=\"right\" nowrap=\"nowrap\"><b>You Save:<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><span class=\"littleorange\">(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5})) ((\()([0-9][0-9](\%))(\)))<\/span><\/td><\/tr>"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    SavingPercent[i] = match.group(8)
-    i=i+1
-
-
-for j in range(len(LastPrice)):
-    dataObject = {
-        "Description" : Description[j],
-        "Title:" : Title[j],
-        "LastPrice": LastPrice[j],
-        "Price": Price[j],
-        "Saving": Saving[j],
-        "SavingPercent": SavingPercent[j]
-    }
-    print("Output object:\n%s" % json.dumps(dataObject, indent = 4))
-	
-	pass
-
-
-# Extract data from rtvslo.si
-def processRtvslo(html_content):
-	# Import libraries
-
 import re
 import json
 from unidecode import unidecode
 from bs4 import BeautifulSoup
 import string
 
-
-# We rather use the locally-cached file as it may have changed online.
-pageContent = open(html_content, 'r').read()
-pageContent = BeautifulSoup(pageContent, 'html.parser')
-
-# # TITLE
-regex = r"<h1>(.*)<\/h1>"
-Title = re.compile(regex)
-match = Title.search(str(pageContent))
-Title=str(match.group(1))
-#print(Title)
+# Extract data from overstock.com
+def processOverstock(html_content):
+	# Import libraries
 
 
-# # AUTHOR AND PUBLISHED TIME
-regex = r"<div class=\"author-timestamp\">\s*\n?[<strong>]+(.*)<\/strong>\|(.*)"
-Author = re.compile(regex)
-match = Author.search(str(pageContent))
-#print("Author:", match.group(1))
-Author=str(match.group(1))
-#print("PublishedTime",match.group(2))
-PublishedTime=match.group(2)
+    # We rather use the locally-cached file as it may have changed online.
+    pageContent = html_content
+    Description={}
+    LastPrice={}
+    Price={}
+    Saving={}
+    SavingPercent={}
+    Title={}
+    i=0
+
+    #TITLE
+    i=0
+    regex = r"<a href=(\")http:(\/\/)www.overstock.com(\/)cgi-bin(\/)d2.cgi(\?)PAGE=PROFRAME(\&)amp;PROD_ID=(\d{5,7})(\")><b>(.*)</b>"
+    matches = re.finditer(regex, pageContent)
+    for match in matches:
+        Title[i] = match.group(9)
+        i=i+1
+
+    #DESCRIPTION
+    i=0
+    regex=r"(?:<td valign=\"top\"><span class=\"normal\">)((.*?)[\s\S]*?)<br>(.*?)\"><span class=\"tiny\"><b>(.*)<\/b>"
+    matches = re.finditer(regex, pageContent)
+    for match in matches:
+        Description[i] = str(match.group(1)+match.group(4)).replace("\n","")
+        i=i+1
+
+    #LAST PRICE
+    i=0
+    regex=r"<tbody><tr><td align=\"right\" nowrap=\"nowrap\"><b>(.*)<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><s>(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5}))<\/s><\/td><\/tr>"
+    matches = re.finditer(regex, pageContent)
+    for match in matches:
+        LastPrice[i] = match.group(2)
+        i=i+1
 
 
-# # SUBTITLE
-regex = r"<div class=\"subtitle\">(.*)<\/div>"
-Subtitle = re.compile(regex)
-match = Subtitle.search(str(pageContent))
-#print("Subtitle:", match.group(1))
-Subtitle=str(match.group(1))
-# # LEAD
-regex = r"<p class=\"lead\">(.*)<\/p>"
-Lead = re.compile(regex)
-match = Lead.search(str(pageContent))
-# print("Lead:", match.group(1))
-Lead=match.group(1)
+    #REGULAR PRICE
+    i=0
+    regex=r"<tr><td align=\"right\" nowrap=\"nowrap\"><b>Price:<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><span class=\"bigred\"><b>(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5}))<\/b><\/span><\/td><\/tr>"
+    matches = re.finditer(regex, pageContent)
+    for match in matches:
+        Price[i] = match.group(1)
+        i=i+1
 
-#DESCRIPTION
-i=1
-Description={}
-regex=r"(<div style=\"position:absolute\; left: -1000px\; top: -1000px\;\"><img (.*?)<p>)*?(<p class=\"Body\">(.*?)<iframe (.*?)<\/p>)?<p>(.*?)(<\/p>)"
-DEscription = re.compile(regex)
-match = DEscription.search(str(pageContent))
-if(str(match.group(4))==None):
-    pass
-else:
-    Description[i]=match.group(4)
-    i=i+1
-matches = re.finditer(regex, str(pageContent))
-for match in matches:
-    Description[i] = str(match.group(6))      
-    i=i+1
-Dummy=""
-j=1
-while(j<len(Description)): 
-    if(Description[j] is None):
+    #SAVED
+    i=0
+    regex=r"<tr><td align=\"right\" nowrap=\"nowrap\"><b>You Save:<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><span class=\"littleorange\">(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5})) (\()[0-9][0-9](\%)(\))<\/span><\/td><\/tr>"
+    matches = re.finditer(regex, pageContent)
+    for match in matches:
+        Saving[i] = match.group(1)
+        i=i+1
+
+    #SAVED IN PERCENT
+    i=0
+    regex=r"<tr><td align=\"right\" nowrap=\"nowrap\"><b>You Save:<\/b><\/td><td align=\"left\" nowrap=\"nowrap\"><span class=\"littleorange\">(\$(\d{1,5})(\,)?(\d{1,5})?\.(\d{1,5})) ((\()([0-9][0-9](\%))(\)))<\/span><\/td><\/tr>"
+    matches = re.finditer(regex, pageContent)
+    for match in matches:
+        SavingPercent[i] = match.group(8)
+        i=i+1
+
+    records = []
+    for j in range(len(LastPrice)):
+        dataObject = {
+            "Title" : Title[j],
+            "ListPrice": LastPrice[j],
+            "Price": Price[j],
+            "Saving": Saving[j],
+            "SavingPercent": SavingPercent[j],
+            "Content" : Description[j]
+        }
+        records.append(dataObject)
+    print("Overstock output object:\n%s\n" % json.dumps(records, indent = 4))
+        
+
+
+# Extract data from rtvslo.si
+def processRtvslo(html_content):
+	# Import libraries
+
+
+
+
+    # We rather use the locally-cached file as it may have changed online.
+    pageContent = html_content
+    pageContent = BeautifulSoup(pageContent, 'html.parser')
+
+    # # TITLE
+    regex = r"<h1>(.*)<\/h1>"
+    Title = re.compile(regex)
+    match = Title.search(str(pageContent))
+    Title=str(match.group(1))
+    #print(Title)
+
+
+    # # AUTHOR AND PUBLISHED TIME
+    regex = r"<div class=\"author-timestamp\">\s*\n?[<strong>]+(.*)<\/strong>\|(.*)"
+    Author = re.compile(regex)
+    match = Author.search(str(pageContent))
+    #print("Author:", match.group(1))
+    Author=str(match.group(1))
+    #print("PublishedTime",match.group(2))
+    PublishedTime=match.group(2)
+
+
+    # # SUBTITLE
+    regex = r"<div class=\"subtitle\">(.*)<\/div>"
+    Subtitle = re.compile(regex)
+    match = Subtitle.search(str(pageContent))
+    #print("Subtitle:", match.group(1))
+    Subtitle=str(match.group(1))
+    # # LEAD
+    regex = r"<p class=\"lead\">(.*)<\/p>"
+    Lead = re.compile(regex)
+    match = Lead.search(str(pageContent))
+    # print("Lead:", match.group(1))
+    Lead=match.group(1)
+
+    #DESCRIPTION
+    i=1
+    Description={}
+    regex=r"(<div style=\"position:absolute\; left: -1000px\; top: -1000px\;\"><img (.*?)<p>)*?(<p class=\"Body\">(.*?)<iframe (.*?)<\/p>)?<p>(.*?)(<\/p>)"
+    DEscription = re.compile(regex)
+    match = DEscription.search(str(pageContent))
+    if(str(match.group(4))==None):
+        pass
+    else:
+        Description[i]=match.group(4)
+        i=i+1
+    matches = re.finditer(regex, str(pageContent))
+    for match in matches:
+        Description[i] = str(match.group(6))      
+        i=i+1
+    Dummy=""
+    j=1
+    while(j<len(Description)): 
+        if(Description[j] is None):
+            j=j+1
+            continue
+        Dummy=str(Dummy)+Description[j]
         j=j+1
-        continue
-    Dummy=str(Dummy)+Description[j]
-    j=j+1
-Titl=str(Dummy).split('mesec')
-Description=str((Titl[0]+"mesec").replace("<p>","").replace("</p>","").replace("<strong>","").replace("</strong>","").replace("<p class=\"Body\">","").replace("<br>","").replace("<br/>","").replace("<sub>","").replace("</sub>",""))
-# print(Description)
-dataObject = {
-        "Description" : unidecode(Description),        
-        "Author:" : Author,
+    Titl=str(Dummy).split('mesec')
+    Description=str((Titl[0]+"mesec").replace("<p>","").replace("</p>","").replace("<strong>","").replace("</strong>","").replace("<p class=\"Body\">","").replace("<br>","").replace("<br/>","").replace("<sub>","").replace("</sub>",""))
+    # print(Description)
+    dataObject = {     
+        "Author" : Author,
         "PublishedTime": ' '.join(PublishedTime.split()),
         "Title": Title,
         "SubTitle": Subtitle,
-        "Lead": unidecode(Lead)
+        "Lead": unidecode(Lead),
+        "Content" : unidecode(Description)
     }
-print("Output object:\n%s" % json.dumps(dataObject, indent = 4))
+    print("Rtvslo output object:\n%s\n" % json.dumps(dataObject, indent = 4))
 
-
-	pass
 
 
 # Extract data from manazara.si
 def processManazara(html_content):
-	# Import libraries
+    # Does not work
+    return
 
-import re
-import json
-from unidecode import unidecode
-pageContent = open(html_content ,encoding="utf8").read()
-#print(pageContent)
+	# # Import libraries
+    # pageContent = html_content
+    # #print(pageContent)
 
-# CATEGORY
-regex = r"<li class=\"home\">[\s\S]*?<a (.*?)>(.*?)<\/a>[\s\S]*?<strong>(.*?)<\/strong>"
-Category = re.compile(regex)
-match = Category.search(pageContent)
-CATEGORY=match.group(2)+ "/"+ match.group(3)
-# print("Category:", match.group(2)+"/"+match.group(3))
+    # # CATEGORY
+    # regex = r"<li class=\"home\">[\s\S]*?<a (.*?)>(.*?)<\/a>[\s\S]*?<strong>(.*?)<\/strong>"
+    # Category = re.compile(regex)
+    # match = Category.search(pageContent)
+    # CATEGORY=match.group(2)+ "/"+ match.group(3)
+    # # print("Category:", match.group(2)+"/"+match.group(3))
 
-#NOT GOOD
-# i=0
-# Title={}
-# regex = r"<h2 class=\"product-name\" (.*) title(.*?)>(.*?)<\/a>[\s\S]*?[\s\S]*?(<p class=\"old-price\">[\s]+?<span class=\"price-label\">(.*?)<\/span>[\s]*<span class=\"price\" (.*?)>[\s]+(.*?)<\/span>)+?[\s\S]*?(<p class=\"special-price\">[\s\S]+?<span class=\"price\" (.*?)>[\s]+(.*?)<\/span>)+?[\s\S]*?(<p class=\"old-price regular-price-old-price-container\"><\/p>[\s]+<span class=\"regular-price\"(.*?)>[\s\S]+?<span class=\"price\">(.*?)<\/span>)+?<div class=\"actions\">"
-# matches = re.finditer(regex, pageContent)
-# for match in matches:
-#     Title[i]=str(match.group(10)) + match.group(13)
-#     print(Title[i])
-#     i=i+1
-
-
-i=0
-PRODUCTNAME={}
-#  PRODUCT NAME
-regex = r"<h2 class=\"product-name\" (.*) title(.*?)>(.*?)<\/a>"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    PRODUCTNAME[i]=str(match.group(3))
-    i=i+1
-
-# SIZES
-SIZES={}
-i=0
-regex = r"(<span class=\"size\">(.*)<\/span>)+?"
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    SIZES[i] = str(match.group(2)).replace("<span class=\"size\">"," ").replace("</span>", " ")
-    # print(SIZES[i])
-    i=i+1
+    # #NOT GOOD
+    # # i=0
+    # # Title={}
+    # # regex = r"<h2 class=\"product-name\" (.*) title(.*?)>(.*?)<\/a>[\s\S]*?[\s\S]*?(<p class=\"old-price\">[\s]+?<span class=\"price-label\">(.*?)<\/span>[\s]*<span class=\"price\" (.*?)>[\s]+(.*?)<\/span>)+?[\s\S]*?(<p class=\"special-price\">[\s\S]+?<span class=\"price\" (.*?)>[\s]+(.*?)<\/span>)+?[\s\S]*?(<p class=\"old-price regular-price-old-price-container\"><\/p>[\s]+<span class=\"regular-price\"(.*?)>[\s\S]+?<span class=\"price\">(.*?)<\/span>)+?<div class=\"actions\">"
+    # # matches = re.finditer(regex, pageContent)
+    # # for match in matches:
+    # #     Title[i]=str(match.group(10)) + match.group(13)
+    # #     print(Title[i])
+    # #     i=i+1
 
 
-# GOOD BUT NOT RELEVANT
-# i=0
-# OLD={}
-# Auth={}
-# #  OLD PRICE
-# regex = r"<span class=\"price\" id=\"old-price-(.*?)>[\s](.*?)<\/span>[\s\S]+?<div class=\"actions\">"
-# matches = re.finditer(regex, pageContent)
-# for match in matches:
-#     OLD[i]=str(match.group(1))
-#     OLD[i]=int(OLD[i][0:(len(OLD[i])-1)])
-#     Auth[i]=str(match.group(2))
-#     print(Auth[i],OLD[i])
-#     i=i+1
-# GOOD BUT NOT RELEVANT
-# i=0
-# Current={}
-# Price={}
-# #  CURRENT PRICE
-# regex = r"<span class=\"price\" id=\"product-price-(.*?)>[\s](.*?)<\/span>[\s\S]+?<div class=\"actions\">"
-# matches = re.finditer(regex, pageContent)
-# for match in matches:
-#     Current[i]=str(match.group(1))
-#     Current[i]=int(Title1[i][0:(len(Title1[i])-1)])
-#     Price[i]=str(match.group(2))
-#     print(Price[i],Title1[i])
-#     i=i+1
-for j in range(len(SIZES)):
-    dataObject = {
-        "Category" : CATEGORY,        
-        "Product Name:" : (PRODUCTNAME[j]),
-        "Sizes": SIZES[j],
+    # i=0
+    # PRODUCTNAME={}
+    # #  PRODUCT NAME
+    # regex = r"<h2 class=\"product-name\" (.*) title(.*?)>(.*?)<\/a>"
+    # matches = re.finditer(regex, pageContent)
+    # for match in matches:
+    #     PRODUCTNAME[i]=str(match.group(3))
+    #     i=i+1
 
-    }
-    print("Output object:\n%s" % json.dumps(dataObject, indent = 4))
+    # # SIZES
+    # SIZES={}
+    # i=0
+    # regex = r"(<span class=\"size\">(.*)<\/span>)+?"
+    # matches = re.finditer(regex, pageContent)
+    # for match in matches:
+    #     SIZES[i] = str(match.group(2)).replace("<span class=\"size\">"," ").replace("</span>", " ")
+    #     # print(SIZES[i])
+    #     i=i+1
 
 
+    # # GOOD BUT NOT RELEVANT
+    # # i=0
+    # # OLD={}
+    # # Auth={}
+    # # #  OLD PRICE
+    # # regex = r"<span class=\"price\" id=\"old-price-(.*?)>[\s](.*?)<\/span>[\s\S]+?<div class=\"actions\">"
+    # # matches = re.finditer(regex, pageContent)
+    # # for match in matches:
+    # #     OLD[i]=str(match.group(1))
+    # #     OLD[i]=int(OLD[i][0:(len(OLD[i])-1)])
+    # #     Auth[i]=str(match.group(2))
+    # #     print(Auth[i],OLD[i])
+    # #     i=i+1
+    # # GOOD BUT NOT RELEVANT
+    # # i=0
+    # # Current={}
+    # # Price={}
+    # # #  CURRENT PRICE
+    # # regex = r"<span class=\"price\" id=\"product-price-(.*?)>[\s](.*?)<\/span>[\s\S]+?<div class=\"actions\">"
+    # # matches = re.finditer(regex, pageContent)
+    # # for match in matches:
+    # #     Current[i]=str(match.group(1))
+    # #     Current[i]=int(Title1[i][0:(len(Title1[i])-1)])
+    # #     Price[i]=str(match.group(2))
+    # #     print(Price[i],Title1[i])
+    # #     i=i+1
 
+    # for j in range(min(len(SIZES), len(PRODUCTNAME))):
+    #     dataObject = {
+    #         "Category" : CATEGORY,        
+    #         "Product Name:" : (PRODUCTNAME[j]),
+    #         "Sizes": SIZES[j],
 
-	pass
-# Extract data from nepremicnine.py
-def processManazara(html_content):
-	# Import libraries
-
-import re
-import json
-from unidecode import unidecode
-
-# We rather use the locally-cached file as it may have changed online.
-pageContent = open(html_content, 'r').read()
-#print(pageContent)
-
-# # TITLE
-regex = r"<div id=\"opis\">[\s\S]*?<!--<h1>(.*?)<\/h1>"
-Title = re.compile(regex)
-match = Title.search(pageContent)
-Title = str(match.group(1))
-# print(Title)
-
-# # SubTitle
-regex = r"<div class=\"kratek\">(.*?)<\/div>"
-SubTitle = re.compile(regex)
-match = SubTitle.search(pageContent)
-SubTitle = str(match.group(1))
-# print(SubTitle)
-
-# # SPECIFIC INFORMATION
-
-regex = r"(<div (class=\"dsc\">)(<strong>)?(.*?)(<\/strong>)?<\/div>(<br>)?)"
-i=0
-Specific={}
-matches = re.finditer(regex, pageContent)
-for match in matches:
-    Specific[i] = unidecode(str(match.group(4))).replace("</strong>", "")
-    # print(Specific[i])
-    i=i+1
-    
-# # GENERAL DESCRIPTION
-regex = r"(<div class=\"opis\">(.*?)[\s\S]*?(?=<\/div>))"
-General = re.compile(regex)
-match = General.search(pageContent)
-General = str(match.group(2)+match.group(1)).replace("<div class=\"opis\">", "")
-General=General.replace("<div>", "")
-General=General.replace("<br>", "")
-General=General.replace("<p>","")
-General=General.replace("</p>","")
-General=General.replace("</strong>","")
-General=General.replace("<strong>","")
-General=General.replace("<ul>","")
-General=General.replace("<li>","")
-General=General.replace("</li>","")
-General=General.replace("</ul>","")
-# print(General)
-
-# # SELLER
-regex = r"(<div class=\"prodajalec\">(<h2>)?(.*?)(<\/h2>)?<\/div>)"
-Seller = re.compile(regex)
-match = Seller.search(pageContent)
-Seller = str(match.group(3))
-# print(Seller)
-
-# # LOCATION
-regex = r"(((<div class=\"naslov\">([\s\S]*?))<\/div>)[\s\S]*?(?=<div class=\"tel\"><\/div>))"
-Location = re.compile(regex)
-match = Location.search(pageContent)
-Location = str(match.group(3).replace("<br>", " "))
-Location= str(Location.replace("<div class=\"naslov\">", ""))
-# print(Location)
-
-dataObject = {
-        "Title" : unidecode(Title),
-        "SubTitle:" : unidecode(SubTitle),
-        "Specific Information": Specific,
-        "General Information": unidecode(General).replace("\n", "").replace("&nbsp;", " ").replace("\t", " "),
-        "Seller": unidecode(Seller),
-        "Location": unidecode(Location).replace("\n","").strip()
-    }
-print("Output object:\n%s" % json.dumps(dataObject, indent = 4))
-
-pass
+    #     }
+    #     print("Output object:\n%s" % json.dumps(dataObject, indent = 4))
